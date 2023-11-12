0d22e0c1fe0a2b5849bc7cce787e7ce9f7b21fb2
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-11-13 00:33:10 +0200

    add modal functionality

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if(modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if(modal==null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+
 function updateTimer() {
     future = Date.parse("dec 31, 2023 23:59:00");
     now = new Date();
@@ -21,4 +61,3 @@ function updateTimer() {
         '<div>' + s + '<span><br>Seconds</span></div>';
 }
 setInterval('updateTimer()', 1000);
-
